ff9b65d091906619678fcb21e25759b2ed4c17bb
gaston <gaston_a96@hotmail.com>
2022-02-11 23:17:24 -0300

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const productsRouter = require('./routes/products');
+const homeRouter = require('./routes/home');
 
 const PORT = process.env.PORT || 8080;
 
@@ -7,7 +9,13 @@ const app = express();
 
 // JSON 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+//---- Routes ----// 
+
+// Home Router
+app.use('/', homeRouter);
 // Products Router
 app.use('/api/products', productsRouter);
 
